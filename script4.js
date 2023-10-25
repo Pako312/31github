fcac67ce923a5600ae02a285068a123a0ddf7aca
Timur <tima97855@gmail.com>
2023-10-25 09:53:01 +0600

    First commit

---
+++ script4.js
@@ -0,0 +1,37 @@
+'use strict'
+// 1)Напишите скрипт, который считает количество секунд в часе.
+
+// var secondsInAMinute = 60;
+// var minutesInAnHour = 60;
+// var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+
+// 3) В переменной day лежит какое-то число из интервала от 1 до 31. 
+// Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+
+// let day = 16;
+// if (day > 0 && day <= 9) {
+// alert('Now is the first decade');
+// } else if (day >= 10 && day < 20) {
+// alert('Now is the second decade');
+// } else {
+// alert('Now is the third decade');
+// };
+
+// 4) В переменной month лежит какое-то число из интервала от 1 до 12. 
+// Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+
+// let month = 1;
+// if (month >= 1 && month < 3) {
+// alert('It is winter now.');
+// } else if (month >= 3 && month < 6) {
+// alert('It is spring now.')
+// } else if (month >= 6 && month < 9) {
+// alert('It is summer now.')
+// } else if (month >= 9 && month <= 11) {
+// alert('It is fall now.');
+// } else if (month === 12) {
+// alert('It is winter now.');
+// } else {
+// alert('Error!');
+// };
+
