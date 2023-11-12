de532546d120a57e1ed2aac55d8ae527a8e70748
Pako312 <101926092+Pako312@users.noreply.github.com>
2023-11-12 20:38:30 +0600

    Add files via upload

---
+++ index_3.js
@@ -0,0 +1,35 @@
+'use strict'
+// window.onbeforeunload = function() {
+//     return false;
+//   };
+
+//   window.onbeforeunload = function() {
+//     return "Есть несохранённые изменения. Всё равно уходим?";
+//   };
+// setTimeout(function greet() {
+//     console.log('Hello!')
+//   }, 5000)
+  
+//   console.log("I'm being called before greet function.")
+  
+
+// добавим в localStorage ключ «bgColor» со значением «green»
+// localStorage.setItem('bgColor', 'green');
+// // получим значение ключа «bgColor» и сохраним его в переменную «bgColor»
+// const bgColor = localStorage.getItem('bgColor');
+// // удалим ключ «bgColor»
+
+// localStorage.removeItem('bgColor');
+
+// console.log(bgColor)
+
+// localStorage['bgColor'] = 'blue';
+// const bgColor = localStorage['bgColor'];
+// // // delete localStorage['bgColor'];
+// // console.log(bgColor)
+// for(let  i = 0, length = localStorage.length; i < length; i++){
+//     const key = localStorage.key(i);
+//     const value = localStorage[key];
+//     console.log(`${key}: ${value}`);
+// }
+// localStorage.clear();
