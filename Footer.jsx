8d737bca4cc6db1e4e9f371623238deb8349653c
Pako312 <101926092+Pako312@users.noreply.github.com>
2023-11-25 00:21:42 +0600

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Index.css';
+function Footer() {
+    return (
+        <div className="Site-footer">
+            <b>Footer</b>
+            <p>all rights reserved 2023</p>
+        </div>
+    );
+};
+
+
+export default Footer;
