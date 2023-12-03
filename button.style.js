dda211e0b0534fe3942a6cf92b498fcb7f851a48
Pako312 <101926092+Pako312@users.noreply.github.com>
2023-12-03 09:04:05 +0600

    Add files via upload

--- button.style.js
+++ button.style.js
@@ -5,10 +5,7 @@ padding: 10px;
 background-color:rgb(0, 106, 255);
 font-size: 20px;
 font-weight:bold;
-color:${(props) => props.color };
+color:#fff;
 border-radius: 25px;
 margin-top:10px;
-&:active {
-    background-color:coral
-}
 `
